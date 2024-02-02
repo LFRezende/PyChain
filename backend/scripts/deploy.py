1593ce38069e1c7aa51929d5bf28371ed5eaa2a0
LFRezende <lfsrsprofessional@gmail.com>
2024-02-02 05:17:39 -0300

    Flask connected the blockchain

--- backend/scripts/deploy.py
+++ backend/scripts/deploy.py
@@ -1,13 +1,21 @@
 from backend.structures.chain import Block, Blockchain
 import datetime as date
+
+def tx(amount, payer, receiver):
+    message = f"  {str(payer).capitalize()} -> {str(receiver.capitalize())}: {amount:.1f} PTC  "
+    return message.center(len(message) + 28)    
+
+
 NONCE = 1
 
 blockchain = Blockchain()
 
-blockchain.add_block(Block(NONCE, date.datetime.now(),"10 BTC de Alice para Bob"))
-blockchain.add_block(Block(NONCE + 1, date.datetime.now(),"10 BTC de Charlie para Bob"))
-blockchain.add_block(Block(NONCE + 2, date.datetime.now(),"10 BTC de Daniel para Bob"))
-blockchain.add_block(Block(NONCE + 3, date.datetime.now(),"10 BTC de Ezekiel para Bob"))
-blockchain.add_block(Block(NONCE + 4, date.datetime.now(),"10 BTC de Ferdinand para Bob"))
+
+
+blockchain.add_block(Block(NONCE, date.datetime.now(),tx(5, "Alice", "Senai")))
+blockchain.add_block(Block(NONCE + 1, date.datetime.now(),tx(10, "Memelo", "Zezélia")))
+blockchain.add_block(Block(NONCE + 2, date.datetime.now(),tx(1.5, "Zézélia", "Baltor")))
+blockchain.add_block(Block(NONCE + 3, date.datetime.now(),tx(8.5, "Lisa", "Mimira")))
+blockchain.add_block(Block(NONCE + 4, date.datetime.now(),tx(50, "Elfo", "Bixaral")))
 
 blockchain.read_chain()
--- frontend/app.py
+++ frontend/app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, url_for, request, redirect
 from backend.structures.chain import Block, Blockchain
+from backend.scripts.deploy import blockchain, NONCE
 app = Flask(__name__)
 
 @app.route('/')
 
 def index():
-    return render_template('index.html')
+    return render_template('index.html', chain = blockchain.chain)
 
 if __name__ == "__main__":
     app.run(debug=True)
