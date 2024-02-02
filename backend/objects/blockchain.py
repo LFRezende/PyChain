25a01bcdbaffb7f15e3af82bc177d0cd9543513e
LFRezende <lfsrsprofessional@gmail.com>
2024-02-02 00:24:42 -0300

    Block defined

--- backend/objects/blockchain.py
+++ backend/objects/blockchain.py
@@ -1,5 +1,5 @@
 import hashlib
-import datetime as datetime
+import datetime as date
 
 class Block:
     def __init__(self, index, timestamp, data, previous_hash):
@@ -19,3 +19,36 @@ class Block:
         block_raw = nonce + blockTime + blockData + prevHash
 
         return hashlib.sha256(block_raw.encode()).hexdigest()
+
+
+class Blockchain:
+    def __init__(self):
+        self.chain = [self.genesis()]
+    
+    def genesis(self):
+        return Block(0, date.datetime.now(), "T3 Scored - 41 Remaining.")
+
+    def last_block(self):
+        return self.chain[-1]
+    
+    def add_block(self, next_block):
+        # Self aqui é para a blockchain, não o bloco.
+        # O previous hash sofre overwriting, mesmo que ele já tenha.
+        next_block.previous_hash = self.last_block().hash
+        self.chain.append(next_block)
+    
+    def check_chain(self):
+        for j in range(1, len(self.chain)):
+            current_block = self.chain[j]
+            prev_block = self.chain[j - 1]
+
+            if current_block.hash != current_block.blockHash():
+                message = f"### Error in Hashing ###\n Block no.{current_block.index} hashes to {current_block.hash}, yet should be {current_block.blockHash()}."
+                print(message)
+                return (False, message)
+            
+            if current_block.previous_hash != prev_block.hash:
+                message = f"### MISMATCH ###\n Block no.{current_block.index} mismatches with Block no.{prev_block.index}"
+                print(message)
+                return (False, message)
+        return (True, "### Chain Verified ###")
