2a7f4bea52a8d5c791a8fd01a9a9427161f1c739
LFRezende <lfsrsprofessional@gmail.com>
2024-02-02 05:46:06 -0300

    Wallets can be created

--- backend/scripts/deploy.py
+++ backend/scripts/deploy.py
@@ -1,14 +1,29 @@
 from backend.structures.chain import Block, Blockchain
 import datetime as date
+import hashlib
 
 def tx(amount, payer, receiver):
     message = f"  {str(payer).capitalize()} -> {str(receiver.capitalize())}: {amount:.1f} PTC  "
-    return message.center(len(message) + 28)    
+    return message.center(len(message) + 28) 
+
+def store():
+    Block()
 
 
-NONCE = 1
 
 blockchain = Blockchain()
+NONCE = 1
+
+print("--- Select your option: --- ")
+op = int(input(" _-_-_-_-_- \n1 - Register your name and get a wallet.\n 2 - Pay some PTC to some friend\n 3- End.\n  _-_-_-_-_- "))
+if op == 1:
+    name = str(input("Input your name")).capitalize().strip()
+    time = date.datetime.now()
+    name = name + str(NONCE) + str(time)
+    wallet = "0x" +  hashlib.sha256(name.encode()).hexdigest()
+    blockchain.add_block(Block(NONCE, time , f"Wallet created: {wallet[0:8]} ..."))
+    NONCE = NONCE + 1
+
 
 
 
@@ -19,3 +34,6 @@ blockchain.add_block(Block(NONCE + 3, date.datetime.now(),tx(8.5, "Lisa", "Mimir
 blockchain.add_block(Block(NONCE + 4, date.datetime.now(),tx(50, "Elfo", "Bixaral")))
 
 blockchain.read_chain()
+
+
+
