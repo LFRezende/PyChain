704be5a8bd8cd381a0576f828a7efcc252ce5c99
LFRezende <lfsrsprofessional@gmail.com>
2024-02-03 04:00:09 -0300

    Add: Inputs for transfer button

--- frontend/app.py
+++ frontend/app.py
@@ -3,25 +3,29 @@ from backend.structures.chain import Block, Blockchain
 from backend.scripts.functions import *
 app = Flask(__name__)
 blockchain = Blockchain()
-Ok = False
+Ok_Wallet = False
+txRequest = False
 @app.route('/', methods = ['POST', 'GET'])
 
 def index():
     if request.method == 'POST':
-        global Ok
-        Ok = False
+        global Ok_Wallet
+        global txRequest
+        Ok_Wallet = False
+        txRequest = False
         if "button_wallet" in request.form:
             createWalletWeb(blockchain.nonce, blockchain, "")
-        if "button_tx" in request.form:
-            return "Not avaiable yet :("
         if "button_allWallets" in request.form:
-            Ok = True
+            Ok_Wallet = True
+        if "button_tx" in request.form:
+            txRequest = True
+
         #noTerminal(globalNonce, blockchain)
         #nonce = simTerminal(NONCE, blockchain)
         return redirect("/")
         
     elif request.method == "GET":
-        return render_template('index.html', chain = blockchain.chain, ok = Ok, wallets = blockchain.wallets)
+        return render_template('index.html', chain = blockchain.chain, ok = Ok_Wallet, txRequest = txRequest, wallets = blockchain.wallets)
     
 if __name__ == "__main__":
     app.run(debug=True)
